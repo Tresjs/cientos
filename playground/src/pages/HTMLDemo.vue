38388c6129361077f29c8a8890163e43a3b01a5e
alvarosabu <alvaro.saburido@gmail.com>
2023-07-13 20:22:52 +0200

    feat: correct objectScale and calculatePosition

--- playground/src/pages/HTMLDemo.vue
+++ playground/src/pages/HTMLDemo.vue
@@ -2,6 +2,7 @@
 import { TresCanvas } from '@tresjs/core'
 import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three'
 
+import '@tresjs/leches/styles'
 import { OrbitControls, Html } from '@tresjs/cientos'
 
 const gl = {
@@ -12,15 +13,27 @@ const gl = {
   outputColorSpace: SRGBColorSpace,
   toneMapping: NoToneMapping,
 }
+
+const state = reactive({
+  wrapperClass: 'wrapper',
+  as: 'div',
+  transform: false,
+  center: true,
+})
 </script>
 
 <template>
   <TresCanvas v-bind="gl">
-    <TresPerspectiveCamera :position="[0, 0, 4]" />
+    <TresPerspectiveCamera :position="[3, 0, 4]" />
     <OrbitControls />
-    <Html>
-      <div class="awiwi" style="background: #000; color: #fff; padding: 1rem; border-radius: 0.5rem">Hello, world!</div>
-    </Html>
+
+    <TresMesh :position="[1, 1, 1]">
+      <TresBoxGeometry />
+      <TresMeshNormalMaterial />
+      <Html v-bind="state">
+        <h1 class="awiwi" style="background: #008080; color: #fff; padding: 1rem; border-radius: 0.5rem">Box</h1>
+      </Html>
+    </TresMesh>
     <TresAmbientLight :intensity="1" />
   </TresCanvas>
 </template>
--- src/core/misc/HTML.vue
+++ src/core/misc/HTML.vue
@@ -1,20 +1,39 @@
 <script setup lang="ts">
-import { DoubleSide, Group, PlaneGeometry, PointsMaterial, ShaderMaterial, Vector3 } from 'three'
-import { computed, createVNode, shallowRef, toRefs } from 'vue'
+import { computed, createVNode, shallowRef, toRefs, render, watchEffect, ref, watch } from 'vue'
+import { DoubleSide, Group, OrthographicCamera, PerspectiveCamera, PlaneGeometry, ShaderMaterial, Vector3 } from 'three'
+import { TresCamera, TresObject3D, useRenderLoop } from '@tresjs/core'
 import { useCientos } from '../useCientos'
 import { Mutable } from '@vueuse/core'
-import { watchEffect, h } from 'vue'
-import { TresCamera, TresObject3D } from '@tresjs/core'
-import { render } from 'vue'
+import { nextTick } from 'vue'
 
-const v1 = new Vector3()
+const v1 = new Vector3(0, 0, 0)
+const v2 = new Vector3(0, 0, 0)
+const v3 = new Vector3(0, 0, 0)
 
-function calculatePosition(el: TresObject3D, camera: TresCamera, size: { width: number; height: number }) {
-  const objectPos = v1.setFromMatrixPosition(el.matrixWorld)
+function calculatePosition(instance: TresObject3D, camera: TresCamera, size: { width: number; height: number }) {
+  const objectPos = v1.setFromMatrixPosition(instance.matrixWorld)
   objectPos.project(camera)
   const widthHalf = size.width / 2
   const heightHalf = size.height / 2
-  return [objectPos.x * widthHalf + widthHalf, -(objectPos.y * heightHalf) + heightHalf]
+  return [
+    (Number.isNaN(objectPos.x) ? 0 : objectPos.x) * widthHalf + widthHalf,
+    -(objectPos.y * heightHalf) + heightHalf,
+  ]
+}
+
+function objectScale(el: TresObject3D, camera: TresCamera) {
+  if (camera instanceof OrthographicCamera) {
+    return camera.zoom
+  } else if (camera instanceof PerspectiveCamera) {
+    const objectPos = v1.setFromMatrixPosition(el.matrixWorld)
+    const cameraPos = v2.setFromMatrixPosition(camera.matrixWorld)
+    const vFOV = (camera.fov * Math.PI) / 180
+    const dist = objectPos.distanceTo(cameraPos)
+    const scaleFOV = 2 * Math.tan(vFOV / 2) * dist
+    return 1 / scaleFOV
+  } else {
+    return 1
+  }
 }
 
 export interface HTMLProps {
@@ -24,6 +43,10 @@ export interface HTMLProps {
   transform?: boolean
   portal?: Mutable<HTMLElement>
   wrapperClass?: string
+  eps?: number
+  distanceFactor?: number
+  fullscreen?: boolean
+  center?: boolean
 }
 
 const props = withDefaults(defineProps<HTMLProps>(), {
@@ -33,40 +56,119 @@ const props = withDefaults(defineProps<HTMLProps>(), {
   }),
   as: 'div',
   transform: false,
+  eps: 0.0001,
 })
 
 const slots = defineSlots()
 
-const groupRef = shallowRef<Group>()
+const groupRef = shallowRef<TresObject3D>()
 
-const { geometry, material, as, transform, portal } = toRefs(props)
+const { geometry, material, as, transform, portal, wrapperClass, eps, distanceFactor, fullscreen, center, style } =
+  toRefs(props)
 
 const { state } = useCientos()
 
 const el = computed(() => document.createElement(as.value))
 
-const root = computed(() => state.appContext.appContext.app)
+const previousPosition = ref([0, 0])
+const previousZoom = ref(0)
+
+const styles = computed(() => {
+  if (transform.value) {
+    return {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: state.renderer?.domElement.width,
+      height: state.renderer?.domElement.height,
+      transformStyle: 'preserve-3d',
+      pointerEvents: 'none',
+    }
+  } else {
+    return {
+      position: 'absolute',
+      transform: center.value ? 'translate3d(-50%,-50%,0)' : 'none',
+      ...(fullscreen.value && {
+        top: -state.container.value.offsetHeight / 2,
+        left: -state.container.value.offsetWidth / 2,
+        width: state.container.value.offsetWidth,
+        height: state.container.value.offsetHeight,
+      }),
+      ...style?.value,
+    }
+  }
+})
+
+watch(
+  () => [groupRef.value, state.renderer],
+  ([group, renderer]) => {
+    if (group && renderer) {
+      const target = portal?.value || renderer.domElement
+      state.scene?.updateMatrixWorld()
+
+      if (transform.value) {
+        el.value.style.cssText = `position:absolute;top:0;left:0;pointer-events:none;overflow:hidden;`
+      } else {
+        const vector = calculatePosition(group, state.camera, {
+          width: state.container.value.offsetWidth,
+          height: state.container.value.offsetHeight,
+        })
+        el.value.style.cssText = `position:absolute;top:0;left:0;transform:translate3d(${vector[0]}px,${vector[1]}px,0);transform-origin:0 0;`
+      }
+
+      if (target) {
+        target.parentNode?.appendChild(el.value)
+      }
+
+      let vnode: any
+
+      if (transform.value) {
+        el.value.style.width = `${geometry.value.parameters.width}px`
+        el.value.style.height = `${geometry.value.parameters.height}px`
+      } else {
+        vnode = createVNode('div', { ref: 'el', style: styles.value }, slots.default?.())
+      }
+      render(vnode, el.value)
+    }
+  },
+)
 
 watchEffect(() => {
-  if (state.renderer && groupRef.value) {
-    const target = portal?.value || state.renderer.domElement
-    state.scene?.updateMatrixWorld()
-    if (transform.value) {
-      el.value.style.cssText = `position:absolute;top:0;left:0;pointer-events:none;overflow:hidden;`
-    } else {
-      const vec = calculatePosition(groupRef.value, state.camera, {
-        width: state.renderer.domElement.width,
-        height: state.renderer.domElement.height,
-      })
-      el.value.style.cssText = `position:absolute;top:0;left:0;transform:translate3d(${vec[0]}px,${vec[1]}px,0);transform-origin:0 0;`
+  if (wrapperClass?.value) {
+    el.value.className = wrapperClass.value
+  }
+})
+
+const { onLoop } = useRenderLoop()
+
+onLoop(() => {
+  if (groupRef.value && state.camera && state.renderer) {
+    state?.camera?.updateMatrixWorld()
+    groupRef.value.updateWorldMatrix(true, false)
+
+    const vector = transform.value
+      ? previousPosition.value
+      : calculatePosition(groupRef.value, state.camera, {
+          width: state.container.value.offsetWidth,
+          height: state.container.value.offsetHeight,
+        })
+
+    if (
+      transform.value ||
+      Math.abs(previousZoom.value - state.camera.zoom) > eps.value ||
+      Math.abs(previousPosition.value[0] - vector[0]) > eps.value ||
+      Math.abs(previousPosition.value[1] - vector[1]) > eps.value
+    ) {
+      if (transform.value) {
+      } else {
+        const scale =
+          distanceFactor?.value === undefined ? 1 : objectScale(groupRef.value, state.camera) * distanceFactor?.value
+        el.value.style.transform = `translate3d(${vector[0]}px,${vector[1]}px,0) scale(${scale})`
+      }
     }
 
-    if (target) {
-      target.parentNode?.appendChild(el.value)
-    }
-
-    const vnode = createVNode('div', { ref: 'el' }, slots.default?.())
-    render(vnode, el.value)
+    previousPosition.value = vector
+    previousZoom.value = state.camera.zoom
   }
 })
 
@@ -76,15 +178,15 @@ const shaders = computed(() => ({
     : /* glsl */ `/*
     This shader is from the THREE's SpriteMaterial.
     We need to turn the backing plane into a Sprite
-    (make it always face the camera) if "transfrom" 
-    is false. 
+    (make it always face the camera) if "transfrom"
+    is false.
   */
   #include <common>
 
   void main() {
     vec2 center = vec2(0., 1.);
     float rotation = 0.0;
-    
+
     // This is somewhat arbitrary, but it seems to work well
     // Need to figure out how to derive this dynamically if it even matters
     float size = 0.03;
@@ -116,7 +218,7 @@ const shaders = computed(() => ({
 <template>
   <TresGroup ref="groupRef">
     <TresMesh :geometry="geometry">
-      <primitive :object="material" :vertex-shader="shaders.vertexShader" :fragment-shader="shaders.fragmentShader" />
+      <primitive :object="material" :fragment-shader="shaders.fragmentShader" />
     </TresMesh>
   </TresGroup>
 </template>
