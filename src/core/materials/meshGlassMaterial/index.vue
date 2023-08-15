2e7ca62b6f9cbdc822aed542a94c8fa1be8c699f
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2023-08-15 10:54:09 -0400

    feat: add glass material

---
+++ src/core/materials/meshGlassMaterial/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { shallowRef } from 'vue'
+import { useTresContext } from '@tresjs/core'
+
+import MeshGlassMaterial from './material'
+
+const materialRef = shallowRef()
+
+const { extend } = useTresContext()
+
+extend({ MeshGlassMaterial })
+
+defineExpose({ materialRef })
+</script>
+<template>
+  <TresMeshGlassMaterial ref="materialRef" v-bind="$attrs" />
+</template>
