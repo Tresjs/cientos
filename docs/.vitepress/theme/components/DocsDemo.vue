5b9a530e9e59b026e684d47a1690d7c81e1b163c
alvarosabu <alvaro.saburido@gmail.com>
2023-09-15 18:50:26 +0200

    docs: add client only again

--- docs/.vitepress/theme/components/DocsDemo.vue
+++ docs/.vitepress/theme/components/DocsDemo.vue
@@ -2,12 +2,14 @@
 </script>
 
 <template>
-  <div
-    class="relative aspect-video"
-    style=" height: auto; margin: 2rem 0; border-radius: 8px; overflow:hidden;"
-  >
-    <Suspense>
-      <slot />
-    </Suspense>
-  </div>
+  <ClientOnly>
+    <div
+      class="relative aspect-video"
+      style=" height: auto; margin: 2rem 0; border-radius: 8px; overflow:hidden;"
+    >
+      <Suspense>
+        <slot />
+      </Suspense>
+    </div>
+  </ClientOnly>
 </template>
