97ba4722fdc4ed06b2c3c281ec131e135b157d6f
Francesco Michelini <hello@francescomichelini.com>
2023-09-21 22:07:16 +0200

    Lint

--- playground/src/pages/abstractions/FBODemo.vue
+++ playground/src/pages/abstractions/FBODemo.vue
@@ -5,16 +5,16 @@ import { Text3D, OrbitControls, useFBO } from '@tresjs/cientos'
 import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three'
 
 const gl = {
-	clearColor: '#82DBC5',
-	shadows: true,
-	alpha: false,
-	shadowMapType: BasicShadowMap,
-	outputColorSpace: SRGBColorSpace,
-	toneMapping: NoToneMapping,
+  clearColor: '#82DBC5',
+  shadows: true,
+  alpha: false,
+  shadowMapType: BasicShadowMap,
+  outputColorSpace: SRGBColorSpace,
+  toneMapping: NoToneMapping,
 }
 
-const fontPath =
-	'https://raw.githubusercontent.com/Tresjs/assets/main/fonts/FiraCodeRegular.json'
+const fontPath
+	= 'https://raw.githubusercontent.com/Tresjs/assets/main/fonts/FiraCodeRegular.json'
 
 const reactiveText = ref('You can edit me')
 
@@ -22,24 +22,24 @@ const fbo = useFBO({ depth: true })
 </script>
 
 <template>
-	<div class="input-center">
-		<input v-model="reactiveText" />
-	</div>
-	<TresCanvas v-bind="gl">
-		<TresPerspectiveCamera :position="[0, 0.5, 5]" />
-		<OrbitControls />
-		<Suspense>
-			<Text3D
-				:text="reactiveText"
-				:size="0.3"
-				:font="fontPath"
-				center
-				:need-updates="true"
-			/>
-		</Suspense>
-		<TresGridHelper :args="[10, 10]" />
-		<TresAmbientLight :intensity="1" />
-	</TresCanvas>
+  <div class="input-center">
+    <input v-model="reactiveText">
+  </div>
+  <TresCanvas v-bind="gl">
+    <TresPerspectiveCamera :position="[0, 0.5, 5]" />
+    <OrbitControls />
+    <Suspense>
+      <Text3D
+        :text="reactiveText"
+        :size="0.3"
+        :font="fontPath"
+        center
+        :need-updates="true"
+      />
+    </Suspense>
+    <TresGridHelper :args="[10, 10]" />
+    <TresAmbientLight :intensity="1" />
+  </TresCanvas>
 </template>
 
 <style scoped>
