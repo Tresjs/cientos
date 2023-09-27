97c733b0bfc5255e8419fecc7f128479f32d904e
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2023-09-27 15:16:42 -0300

    chore: remove comments in demo

--- playground/src/pages/abstractions/ReflectorMeshDemo.vue
+++ playground/src/pages/abstractions/ReflectorMeshDemo.vue
@@ -23,42 +23,6 @@ const reflectorRef = shallowRef()
 watch(reflectorRef, (value) => {
   console.log(value)
 })
-// const customShader = {
-//   name: 'customShader',
-
-//   fragmentShader: /* glsl */ `
-// 		uniform vec3 color;
-// 		uniform sampler2D tDiffuse;
-// 		varying vec4 vUv;
-
-// 		#include <logdepthbuf_pars_fragment>
-
-// 		float blendOverlay( float base, float blend ) {
-
-// 			return( base < 0.5 ? ( 2.0 * base * blend ) : ( 1.0 - 2.0 * ( 1.0 - base ) * ( 1.0 - blend ) ) );
-
-// 		}
-
-// 		vec3 blendOverlay( vec3 base, vec3 blend ) {
-
-// 			return vec3(
-//                 blendOverlay( base.r, blend.r ), blendOverlay( base.g, blend.g ), blendOverlay( base.b, blend.b ) 
-//                 );
-
-// 		}
-
-// 		void main() {
-
-// 			#include <logdepthbuf_fragment>
-
-// 			vec4 base = texture2DProj( tDiffuse, vUv );
-// 			gl_FragColor = vec4( blendOverlay( base.rgb, color ), 1.0 );
-
-// 			#include <tonemapping_fragment>
-// 			#include <colorspace_fragment>
-
-// 		}`,
-// }
 
 const options = {
   color: '#f7f7f7',
@@ -94,8 +58,6 @@ const options = {
       :texture-width="options.textureWidth"
       :texture-height="options.textureHeight"
     />
-    <!-- <TresBoxGeometry :args="[10, 10, 1]" /> -->
-    <!-- <TresBoxGeometry :args="[10, 10]" /> -->
     <TresAmbientLight :intensity="1" />
     <OrbitControls />
   </TresCanvas>
