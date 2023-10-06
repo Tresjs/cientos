16ebffb6bea1f7129b8678d7d084c43838cfd3ea
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2023-10-06 10:34:02 -0300

    chore: handle error null ref for vAlwaysLookAt

--- src/core/directives/vAlwaysLookAt.ts
+++ src/core/directives/vAlwaysLookAt.ts
@@ -1,10 +1,16 @@
-import { useRenderLoop } from '@tresjs/core'
+import { useRenderLoop, useLogger } from '@tresjs/core'
 import type { Object3D } from 'three'
 import { extractBindingPosition } from '../../utils/index'
+
+const { logWarning } = useLogger()
 
 export const vAlwaysLookAt = {
   updated: (el: Object3D, binding: any) => {
     const observer = extractBindingPosition(binding)
+    if (!observer) {
+      logWarning(`${binding} is not a Object3D`)
+      return
+    }
     const { onLoop } = useRenderLoop()
     onLoop(() => {
       el.lookAt(observer)
