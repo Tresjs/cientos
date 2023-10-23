8e2465778595d4ba02bf6e6aa7ecad4dc22432da
Peter <petermoldovia@yahoo.ca>
2023-10-23 12:59:52 +0200

    feat(Sparkles): fix index.ts error

--- src/core/staging/index.ts
+++ src/core/staging/index.ts
@@ -1,7 +1,6 @@
 import Environment from './useEnvironment/component.vue'
 import Backdrop from './Backdrop.vue'
 import ContactShadows from './ContactShadows.vue'
-import { Environment } from './useEnvironment/component'
 import Precipitation from './Precipitation.vue'
 import Sky from './Sky.vue'
 import Smoke from './Smoke.vue'
