aeae767f9841a0f333f02374ceb079941824a209
Peter <petermoldovia@yahoo.ca>
2023-10-30 06:23:54 +0100

    feat(Superformula): add superformula

---
+++ docs/.vitepress/theme/components/SuperformulaDemo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+import { Superformula, OrbitControls } from '@tresjs/cientos'
+</script>
+
+<template>
+  <TresCanvas clear-color="#777">
+    <Superformula
+      :num-arms-b="24"
+      :exp-b2="30"
+      :exp-b3="20"
+    >
+      <TresMeshNormalMaterial />
+    </Superformula>
+    <OrbitControls />
+  </TresCanvas>
+</template>
