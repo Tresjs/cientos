6d12ba1141e4f01dcefe4c18ba4cca56d4ab7a54
Peter <petermoldovia@yahoo.ca>
2023-12-12 16:20:13 +0100

    chore: fix linter errors

--- playground/src/pages/controls/CameraControlsDemo.vue
+++ playground/src/pages/controls/CameraControlsDemo.vue
@@ -42,20 +42,20 @@ const { distance, minDistance, maxDistance } = useControls({
     value: controlsState.distance,
     min: 0,
     max: 100,
-    step: 0.01
+    step: 0.01,
   },
   minDistance: {
     value: controlsState.minDistance,
     min: 0,
     max: 10,
-    step: 0.01
+    step: 0.01,
   },
   maxDistance: {
     value: controlsState.maxDistance,
     min: 0,
     max: 100,
-    step: 0.01
-  }
+    step: 0.01,
+  },
 })
 
 watch([distance.value, minDistance.value, maxDistance.value], () => {
@@ -66,75 +66,84 @@ watch([distance.value, minDistance.value, maxDistance.value], () => {
 
 useControls(
   'Dolly', {
-  dollyInc: {
-    type: 'button',
-    onClick: () => {
-      controlsRef?.value?.value?.dolly(1, true)
+    dollyInc: {
+      type: 'button',
+      onClick: () => {
+        controlsRef?.value?.value?.dolly(1, true)
+      },
+      label: 'Increment (+1)',
     },
-    label: 'Increment (+1)'
-  },
-  dollyDec: {
-    type: 'button',
-    onClick: () => {
-      controlsRef?.value?.value?.dolly(-1, true)
-    },
-    label: 'Increment (-1)'
-  }})
+    dollyDec: {
+      type: 'button',
+      onClick: () => {
+        controlsRef?.value?.value?.dolly(-1, true)
+      },
+      label: 'Increment (-1)',
+    } })
 
 useControls(
   'Rotate', {
-  rotateTheta45: {
-    type: 'button',
-    label: 'Rotate theta 45°',
-    onClick: () => {
-      controlsRef?.value?.value?.rotate(45 * MathUtils.DEG2RAD, 0, true)
+    rotateTheta45: {
+      type: 'button',
+      label: 'Rotate theta 45°',
+      onClick: () => {
+        controlsRef?.value?.value?.rotate(45 * MathUtils.DEG2RAD, 0, true)
+      },
     },
-  },
-  rotateTheta90: {
-    type: 'button',
-    label: 'Rotate theta -90°',
-    onClick: () => {
-      controlsRef?.value?.value?.rotate(-90 * MathUtils.DEG2RAD, 0, true)
+    rotateTheta90: {
+      type: 'button',
+      label: 'Rotate theta -90°',
+      onClick: () => {
+        controlsRef?.value?.value?.rotate(-90 * MathUtils.DEG2RAD, 0, true)
+      },
     },
-  },
-  rotateTheta360: {
-    type: 'button',
-    label: 'Rotate theta 360°',
-    onClick: () => {
-      controlsRef?.value?.value?.rotate(360 * MathUtils.DEG2RAD, 0, true)
+    rotateTheta360: {
+      type: 'button',
+      label: 'Rotate theta 360°',
+      onClick: () => {
+        controlsRef?.value?.value?.rotate(360 * MathUtils.DEG2RAD, 0, true)
+      },
     },
-  },
-  rotatePhi20: {
-    type: 'button',
-    label: 'Rotate phi 20°',
-    onClick: () => {
-      controlsRef?.value?.value?.rotate(0, 20 * MathUtils.DEG2RAD, true)
+    rotatePhi20: {
+      type: 'button',
+      label: 'Rotate phi 20°',
+      onClick: () => {
+        controlsRef?.value?.value?.rotate(0, 20 * MathUtils.DEG2RAD, true)
+      },
     },
-  },
   })
 
 useControls(
   'Move', {
-  fitToBoundingBox: {
-    type: 'button',
-    label: 'Fit to the bounding box of the mesh',
-    onClick: () => {
-      controlsRef?.value?.value?.fitToBox(boxMeshRef.value, true)
+    fitToBoundingBox: {
+      type: 'button',
+      label: 'Fit to the bounding box of the mesh',
+      onClick: () => {
+        controlsRef?.value?.value?.fitToBox(boxMeshRef.value, true)
+      },
     },
-  },
-})
+  })
 </script>
 
 <template>
   <TresLeches />
   <TresCanvas v-bind="gl">
     <TresPerspectiveCamera :position="[5, 5, 5]" />
-    <CameraControls v-bind="controlsState" ref="controlsRef" make-default @change="onChange" @start="onStart"
-      @end="onEnd" />
+    <CameraControls
+      v-bind="controlsState"
+      ref="controlsRef"
+      make-default
+      @change="onChange"
+      @start="onStart"
+      @end="onEnd"
+    />
     <TresGridHelper :position="[0, -1, 0]" />
     <TresMesh ref="boxMeshRef">
       <TresBoxGeometry :args="[2, 2, 2]" />
-      <TresMeshBasicMaterial color="orange" wireframe />
+      <TresMeshBasicMaterial
+        color="orange"
+        wireframe
+      />
     </TresMesh>
     <TresAmbientLight :intensity="1" />
   </TresCanvas>
