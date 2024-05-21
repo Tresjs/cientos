72284ce2e0e378d353b3573a6216ad6f6dcd4425
Jaime A Torrealba C <63722373+JaimeTorrealba@users.noreply.github.com>
2024-05-21 10:56:46 +0100

    feat: re-remove tweakpane (#425)
    * feat(app)!: 313 re-uninstall tweakpane
    
    * chore: fix lint

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -151,7 +151,6 @@ export default defineConfig({
         text: 'Misc',
         collapsed: true,
         items: [
-          { text: 'useTweakpane', link: '/guide/misc/use-tweakpane' },
           { text: 'Stats', link: '/guide/misc/stats' },
           { text: 'Html', link: '/guide/misc/html-component' },
           { text: 'StatsGl', link: '/guide/misc/stats-gl' },
