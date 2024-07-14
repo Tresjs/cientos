b79492c6ec04676ddf3487d62f01465629f7e86c
alvarosabu <alvaro.saburido@gmail.com>
2024-07-14 18:12:08 +0200

    chore: fix lints

--- src/core/materials/customShaderMaterial/index.vue
+++ src/core/materials/customShaderMaterial/index.vue
@@ -3,9 +3,10 @@ import { shallowRef } from 'vue'
 import { useTresContext } from '@tresjs/core'
 
 import CustomShaderMaterial from 'three-custom-shader-material/vanilla'
+import type { Fn } from '@vueuse/core'
 
 interface CustomShaderMaterialProps {
-  baseMaterial: Function
+  baseMaterial: Fn
   vertexShader?: string
   fragmentShader?: string
   silent?: boolean
