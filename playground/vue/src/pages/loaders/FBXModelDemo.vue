042bd3cda358fbee74cd25673c6c9f8acd8c823e
alvarosabu <alvaro.saburido@gmail.com>
2025-01-05 10:54:42 +0100

    chore(FBXModelDemo): add OrbitControls and adjust FBXModel position and shadow casting

--- playground/vue/src/pages/loaders/FBXModelDemo.vue
+++ playground/vue/src/pages/loaders/FBXModelDemo.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { FBXModel } from '@tresjs/cientos'
+import { FBXModel, OrbitControls } from '@tresjs/cientos'
 import { TresCanvas } from '@tresjs/core'
 import { BasicShadowMap, NoToneMapping, SRGBColorSpace } from 'three'
 
@@ -16,10 +16,13 @@ const gl = {
 <template>
   <TresCanvas v-bind="gl">
     <TresPerspectiveCamera :position="[5.3, 2.45, 9.3]" :look-at="[0, 0, 0]" />
+    <OrbitControls />
     <Suspense>
       <FBXModel
         scale="0.01"
+        :position="[0, -1.6, 0]"
         :rotation-y="-Math.PI * 0.5"
+        cast-shadow
         path="https://raw.githubusercontent.com/Tresjs/assets/main/models/fbx/low-poly-truck/Jeep_done.fbx"
       />
     </Suspense>
