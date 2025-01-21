8bdfc44c52e67c5d5df2787b27c55c518c23f1dd
Peter <petermoldovia@yahoo.ca>
2025-01-21 15:39:59 +0100

    wip

--- src/.vitepress/theme/components/DocsDemoControl.vue
+++ src/.vitepress/theme/components/DocsDemoControl.vue
@@ -12,10 +12,11 @@ const props = defineProps<{ label: string }>()
 <style scoped>
 .control {
   border: 1px solid var(--vp-c-divider);
-  border-top: 0;
+  border-width: 0 1px;
 }
 .control:last-child {
   border-bottom-right-radius: 8px;
   border-bottom-left-radius: 8px;
+  border-bottom-width: 1px;
 }
 </style>
