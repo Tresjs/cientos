2ee2a53d46798b788ae619233780c67beefc7b2f
Peter <petermoldovia@yahoo.ca>
2025-01-25 20:08:42 +0100

    wip

--- src/old/misc/index.ts
+++
@@ -1,7 +0,0 @@
-import { BakeShadows } from './BakeShadows'
-import Html from './html/HTML.vue'
-import { Stats } from './Stats'
-import { StatsGl } from '../../core/StatsGl/component.bue'
-import { useGLTFExporter } from './useGLTFExporter'
-
-export { BakeShadows, Html, Stats, StatsGl, useGLTFExporter }
